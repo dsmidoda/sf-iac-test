b9cf48136cc45548ab712c26977f45163c7fdb72
dsmidoda <67411067+dsmidoda@users.noreply.github.com>
2021-03-08 12:25:08 +0100

    Update create_db.tf

--- src/modules/create_db.tf
+++ src/modules/create_db.tf
@@ -1,4 +1,4 @@
-resource "snowflake_database" "first_tf_db_2" {
+resource "snowflake_database" "first_tf_db" {
   name                        = var.db_name
   comment                     = var.comment
   data_retention_time_in_days = 3
