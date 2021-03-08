dd2923d6620741b0c3ced28ff944c3995dc82b73
dsmidoda <67411067+dsmidoda@users.noreply.github.com>
2021-03-08 12:06:56 +0100

    Create backend.tf

---
+++ src/main/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "new_resource_data_factory"
+    storage_account_name = "snowflakedtest"
+    container_name       = "terraform-state"
+    key                  = "gidif.terraform.tfstate"
+  }
+}
