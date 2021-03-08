408d724cab90000c9c95cb60b37f6516c12e50e3
DSMIDODA <dawidsmidoda027@gmail.com>
2021-03-08 07:41:03 +0100

    update

--- src/main/main.tf
+++ src/main/main.tf
@@ -1,3 +1,28 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "new_resource_data_factory"
+    storage_account_name = "snowflakedtest"
+    container_name       = "terraform-state"
+    key                  = "gidif.terraform.tfstate"
+  }
+}
+
+terraform {
+  required_providers {
+    snowflake = {
+      source = "chanzuckerberg/snowflake"
+      version = "0.23.2"
+    }
+  }
+}
+provider snowflake {
+  username = "dawidsmidoda"
+  account  = "zg95002"
+  region   = "eu-central-1"
+  password = "Dawid32161!"
+  role     = "SYSADMIN"
+}
+
 module "snowflake_setup" {
   source = "../modules"
 }
