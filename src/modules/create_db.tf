41f67f0b7d3b4cd7c143c027c0cce3dcf4d753a6
dsmidoda <67411067+dsmidoda@users.noreply.github.com>
2021-03-08 10:25:24 +0100

    Update create_db.tf

--- src/modules/create_db.tf
+++ src/modules/create_db.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.15.0"
+    }
+  }
+}
+
 resource "snowflake_database" "first_tf_db" {
   name                        = var.db_name
   comment                     = var.comment
